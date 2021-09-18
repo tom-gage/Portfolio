3b373a7d5171b7bec0c6ea661050d8764222822b
Tom Gage <tomgage01@gmail.com>
2021-09-18 22:54:32 +0100

    updates for G

--- messageReader.py
+++ messageReader.py
@@ -6,15 +6,17 @@ import time
 engine = pyttsx3.init()
 
 while True:
-    time.sleep(5)
+    time.sleep(10)
+    
     print("fetching messages...")
-    messages = requests.get('http://localhost:9000/messages')
+    
+    messages = requests.get('https://barno-1.herokuapp.com/messages')
 
     messages = json.loads(messages.text)
 
     for message in messages:
+        print(message)
         engine.say(message)
-
 
     engine.runAndWait()
 
