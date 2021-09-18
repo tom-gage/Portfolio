86f27aea60521a555f57d5b49ce0c95d46d0c438
Tom Gage <tomgage01@gmail.com>
2021-09-18 23:58:55 +0100

    updates for Greenpark

--- server.js
+++ server.js
@@ -12,21 +12,28 @@ app.set('view engine', 'ejs');
 
 let messages = [];
 
+
 app.get('/', function(req, res){
-    res.render('index.ejs');
+    res.render('index.ejs', {
+        messagesList : JSON.stringify(messages)
+    });
 })
+
+
+app.post('/', function(req, res){
+    messages.push(req.body.myInput);
+
+    res.render('index.ejs',{
+        messagesList : JSON.stringify(messages)
+    });
+
+    console.log(messages)
+})
+
 
 app.get('/messages', function(req, res){
     res.send(JSON.stringify(messages));
     messages = [];
-})
-
-app.post('/', function(req, res){
-    res.render('index.ejs');
-
-    messages.push(req.body.myInput);
-
-    console.log(messages)
 })
 
 server.listen(process.env.PORT || 9000, function(req, response){
