407cbb3e60dc126722ec36eadd65a6f2fbe96b86
Tom Gage <tomgage01@gmail.com>
2021-10-27 22:19:30 +0100

    added skeleton for haiku detector

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 const http = require('http');
-const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+const express = require('express')
+const app = express()
 
-const app = express();
+app.use(express.static(__dirname + '/views'))
+app.use('/js/', express.static(path.join(__dirname, 'node_modules/three/build/')))
+app.use('/myThreeCode/', express.static(path.join(__dirname, 'views/3dStuff')))
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const server = http.createServer(app);
 
-//app.set('views', __dirname);
 app.set('view engine', 'ejs');
 
 let messages = [];
 
-
+//index routes
 app.get('/', function(req, res){
     res.render('index.ejs', {
         messagesList : JSON.stringify(messages)
@@ -30,12 +33,18 @@ app.post('/', function(req, res){
     console.log(messages)
 })
 
+//three js routes
+app.get('/3d', function(req, res){
+    res.render('3dStuff/3d.ejs', { 
+        messagesList : JSON.stringify(messages)
+    });
+})
 
+
+//messages api
 app.get('/messages', function(req, res){
     res.send(JSON.stringify(messages));
     messages = [];
 })
 
-server.listen(process.env.PORT || 9000, function(req, response){
-    console.log("listening on port 9000");
-})
+app.listen(9001, () => console.log('Visit http://127.0.0.1:9001'))
